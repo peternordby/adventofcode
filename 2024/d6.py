3c27f83b2e2ccaf10fe3a00e82c238a3e17630cc
Peter Skaar Nordby <peter.nordby@noaignite.com>
2024-12-06 17:23:26 +0100

    Day 6 P2

--- 2024/d6.py
+++ 2024/d6.py
@@ -39,7 +39,57 @@ def part1(parsed):
     return len(visited)
 
 def part2(parsed):
-    pass
+    start = None
+    for i, row in enumerate(parsed):
+        for j, char in enumerate(row):
+            if char in ("^"):
+                start = (i, j)
+
+    max_r = len(parsed[0])
+    max_c = len(parsed)
+
+    loops = 0
+    for i in range(max_r):
+        for j in range(max_c):
+
+            print(i, j)
+
+            parsed_copy = parsed.copy()
+            row = parsed_copy[i]
+            parsed_copy[i] = row[:j] + "#" + row[j+1:]
+
+            pos = start
+
+            if (i, j) == start:
+                continue
+
+            directions = [(-1, 0), (0, 1), (1, 0), (0, -1)] # up, right, down, left
+            direction = 0
+            pos = start
+            next_pos = (pos[0] + directions[direction][0], pos[1] + directions[direction][1])
+
+            visited = set()
+            visited.add((pos, direction))
+
+            while 0 <= next_pos[0] < max_r and 0 <= next_pos[1] < max_c:
+                next_tile = parsed_copy[next_pos[0]][next_pos[1]]
+                if next_tile == "#":
+                    direction = (direction + 1) % 4
+                    next_pos = (pos[0] + directions[direction][0], pos[1] + directions[direction][1])
+                    next_tile = parsed_copy[next_pos[0]][next_pos[1]]
+                    continue
+
+                pos = next_pos
+                if (pos, direction) in visited:
+                    loops += 1
+                    break
+            
+                visited.add((pos, direction))
+
+                next_pos = (pos[0] + directions[direction][0], pos[1] + directions[direction][1])
+
+
+    return loops
 
 if __name__ == '__main__':
     day = int(re.findall(r'\d+', __file__)[-1])
